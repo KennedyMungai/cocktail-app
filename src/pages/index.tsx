5c3530f6f0b5252a3e04b4724542247d664828eb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-24 13:03:47 +0300

    Got rid of the inter font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
 import { Text } from '@chakra-ui/react'
 import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
 	return <Text>Something in the way</Text>
 }
