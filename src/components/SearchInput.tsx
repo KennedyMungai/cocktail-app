63377f7686a0c35a7a04df9098b056d57590b3ee
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-24 15:26:11 +0300

    Added an import for the useRef hook

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,5 +1,5 @@
 import { Input } from '@chakra-ui/react'
-import React from 'react'
+import React, { useRef } from 'react'
 
 const SearchInput = () => {
 	return <Input placeholder='Search Cocktails' width='30rem' />
