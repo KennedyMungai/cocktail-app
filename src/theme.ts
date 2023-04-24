db8e74ffdb070a36d65c34fac90240ec3a7c1066
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-24 14:39:31 +0300

    Added some configuration for the fonts

--- src/theme.ts
+++ src/theme.ts
@@ -2,6 +2,11 @@ import { extendTheme } from '@chakra-ui/react'
 
 const config = { initialColorMode: 'light', useSystemColorMode: false }
 
-const theme = extendTheme({ config })
+const fonts = {
+	heading: `'Open Sans', sans-serif`,
+	body: `'Open Sans', sans-serif`
+}
+
+const theme = extendTheme({ config, fonts })
 
 export default theme
