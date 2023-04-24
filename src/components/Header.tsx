926b67662f66dbf14e328f3bac6dfba8d3018b54
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-24 14:52:28 +0300

    Added a border at the bottom of the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,12 @@ type Props = {}
 
 const Header = (props: Props) => {
 	return (
-		<Flex justifyContent={'center'} gap={4}>
+		<Flex
+			justifyContent={'center'}
+			gap={4}
+			borderBottom={'1px solid #ccc'}
+			p={4}
+		>
 			<Heading as={'h1'} size={'2xl'}>
 				Find Your Favorite Cocktails
 			</Heading>
