574dfa16bfd35c6afd46c43c2c2d9736498a7702
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-24 14:47:31 +0300

    Added the header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
+import Header from '@/components/Header'
 import { Flex, Heading } from '@chakra-ui/react'
 
 export default function Home() {
 	return (
 		<Flex direction={'column'}>
 			<Flex justifyContent={'center'} mb={'4rem'}>
-				<Heading as={'h1'} size={'2xl'}>
-					Cocktail Index
-				</Heading>
+				<Header />
 			</Flex>
 			<Flex></Flex>
 		</Flex>
