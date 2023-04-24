543b47dc979749a12cf0100efe48888914078e94
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-24 14:33:10 +0300

    Made some changes to the index component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
-import { Text } from '@chakra-ui/react'
+import { Flex } from '@chakra-ui/react'
 
 export default function Home() {
-	return <Text>Something in the way</Text>
+	return (
+		<Flex direction={'column'}>
+			<Flex></Flex>
+			<Flex></Flex>
+		</Flex>
+	)
 }
