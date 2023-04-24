1768a2f2dfcfb3d259052aca17d429570db9bcc5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-24 15:31:19 +0300

    Got rid of the props from the component

--- src/components/CocktailList.tsx
+++ src/components/CocktailList.tsx
@@ -2,9 +2,7 @@ import { useAppDispatch, useAppSelector } from '@/app/hooks'
 import { fetchCocktails } from '@/features/posts/postsSlice'
 import React, { useEffect, useState } from 'react'
 
-type Props = {}
-
-const CocktailList = (props: Props) => {
+const CocktailList = () => {
 	return <div>CocktailList</div>
 }
 
