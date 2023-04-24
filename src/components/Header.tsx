7b3a0011f26ddb3d4f138abfab5c601a2b6b1370
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-24 14:48:16 +0300

    Added some markup and styling to the component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,14 @@
+import { Flex, Heading } from '@chakra-ui/react'
 import React from 'react'
 
 type Props = {}
 
 const Header = (props: Props) => {
-	return <div>Header</div>
+	return (
+		<Flex justifyContent={'center'}>
+			<Heading>Find Your Favorite Cocktails</Heading>
+		</Flex>
+	)
 }
 
 export default Header
