a516f8a6275b18608600fca48c5a95efa3136509
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-24 14:36:03 +0300

    Added some props for better styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,9 @@ export default function Home() {
 	return (
 		<Flex direction={'column'}>
 			<Flex justifyContent={'center'} mb={'4rem'}>
-				<Heading>Cocktail Index</Heading>
+				<Heading as={'h1'} size={'2xl'}>
+					Cocktail Index
+				</Heading>
 			</Flex>
 			<Flex></Flex>
 		</Flex>
