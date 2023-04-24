efbbbe8f00606224207bea9fe9e026698bc65662
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-24 15:32:00 +0300

    Initialized useAppSelector and useAppDispatch

--- src/components/CocktailList.tsx
+++ src/components/CocktailList.tsx
@@ -3,6 +3,9 @@ import { fetchCocktails } from '@/features/posts/postsSlice'
 import React, { useEffect, useState } from 'react'
 
 const CocktailList = () => {
+	const {} = useAppSelector((state) => state.posts)
+	const dispatch = useAppDispatch()
+
 	return <div>CocktailList</div>
 }
 
