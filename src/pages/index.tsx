24515b0d260686c36c45afe8c2fc8521103ccaab
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-24 14:35:34 +0300

    Added a margin bottom of 4 rem to the first Flex component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { Flex, Heading } from '@chakra-ui/react'
 export default function Home() {
 	return (
 		<Flex direction={'column'}>
-			<Flex justifyContent={'center'}>
+			<Flex justifyContent={'center'} mb={'4rem'}>
 				<Heading>Cocktail Index</Heading>
 			</Flex>
 			<Flex></Flex>
