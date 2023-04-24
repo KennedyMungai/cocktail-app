3319ddd42cafe116e141c58f6b87f96d4511b73c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-24 15:27:34 +0300

    Gave the useRef a static type

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,7 +2,7 @@ import { Input } from '@chakra-ui/react'
 import React, { useRef } from 'react'
 
 const SearchInput = () => {
-	const searchRef = useRef(null)
+	const searchRef = useRef<HTMLInputElement>(null)
 
 	return (
 		<Input placeholder='Search Cocktails' width='30rem' ref={searchRef} />
