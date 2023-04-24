33791dc4c012e76ab2414f9853e1cca541e4778b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-24 13:06:10 +0300

    Added some exports for the types

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { type } from "os";
 
 
 const store = configureStore({
     reducer: {}
 })
+
+export type RootState = ReturnType<typeof store.getState>
+
+export type AppDispatch = typeof store.dispatch
 
 export default store;
