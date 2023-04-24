2093bc7c57c7ba0918b441922c280c9d3bb00485
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-24 14:56:39 +0300

    Deleted the code on the file and replaced it with a blakn fetchCocktails thunk

--- src/features/posts/postsSlice.ts
+++ src/features/posts/postsSlice.ts
@@ -1,21 +1,7 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import axios from 'axios'
 
-const initialState = {
-	entities: [],
-	loading: false
-}
-
-const getPosts = createAsyncThunk('posts/getPosts', async (thunkApi) => {
-	const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
-	return res.data
-})
-
-const postsSlice = createSlice({
-	name: 'posts',
-	initialState,
-	reducers: {},
-	extraReducers: {}
-})
-
-export default postsSlice.reducer
+export const fetchCocktails = createAsyncThunk(
+	'cocktails/fetchCocktails',
+	async () => {}
+)
