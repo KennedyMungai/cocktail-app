400b6180cd64e8a6024b738d298b6c4f12d14ad9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-25 09:40:17 +0300

    Subdued the reload on form submit

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,8 +1,12 @@
 import { Input } from '@chakra-ui/react'
-import React, { useRef } from 'react'
+import React, { FormEvent, useRef } from 'react'
 
 const SearchInput = () => {
 	const searchRef = useRef<HTMLInputElement>(null)
+
+	const submitHandler = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+	}
 
 	return (
 		<Input
