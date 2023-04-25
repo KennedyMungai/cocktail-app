968d9a977415bb63e71ddab14d229325e3a3298e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-25 09:15:40 +0300

    Defined te newCocktail object

--- src/pages/cocktails/[singlecocktail].tsx
+++ src/pages/cocktails/[singlecocktail].tsx
@@ -38,6 +38,16 @@ const SingleCocktail = () => {
 				strIngredient4,
 				strIngredient5
 			]
+
+			const newCocktail = {
+				name,
+				image,
+				info,
+				category,
+				glass,
+				instructions,
+				ingredients
+			}
 		}
 	}, [singlecocktail, cocktail])
 
