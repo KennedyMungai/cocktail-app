25c8883e2c4bf6307ba80e0889f9101a071c2105
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-25 09:53:09 +0300

    Added a bunch of markup for the component

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,4 +1,14 @@
-import { Input } from '@chakra-ui/react'
+import {
+	Button,
+	Flex,
+	FormControl,
+	FormHelperText,
+	FormLabel,
+	Input,
+	InputGroup,
+	InputLeftAddon,
+	InputRightAddon
+} from '@chakra-ui/react'
 import React, { FormEvent, useRef } from 'react'
 
 const SearchInput = () => {
@@ -9,12 +19,13 @@ const SearchInput = () => {
 	}
 
 	return (
-		<Input
-			placeholder='Search Cocktails'
-			width='30rem'
-			ref={searchRef}
-			onSubmit={submitHandler}
-		/>
+		<FormControl width='40rem' onSubmit={submitHandler}>
+			<InputGroup>
+				<Input type='text' placeholder='Search...' ref={searchRef} />
+				<InputRightAddon children='Search' as={Button} type='submit' />
+			</InputGroup>
+			<FormHelperText>Your cocktail search query</FormHelperText>
+		</FormControl>
 	)
 }
 
