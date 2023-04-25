735d19ad651187cd4cd6e4b840d24438ecb5c7fb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-25 08:52:50 +0300

    Deleted the props from the component

--- src/pages/cocktails/[singlecocktail].tsx
+++ src/pages/cocktails/[singlecocktail].tsx
@@ -3,9 +3,7 @@ import { fetchSingleCocktail } from '@/features/posts/postsSlice'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 
-type Props = {}
-
-const SingleCocktail = (props: Props) => {
+const SingleCocktail = () => {
 	const router = useRouter()
 
 	const { singlecocktail } = router.query
